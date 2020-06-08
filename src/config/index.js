3e8a5060910e03aff4b0aa8af5e9e7e85fe26893
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 20:31:02 -0300

    fix: Adiciona Client Id como variável de ambiente

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,9 @@
 export const config = {
   spotify: {
-    authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: '',
+    authorizationURL: "https://accounts.spotify.com/authorize",
+    clientId: `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`,
     redirectUrl: `${window.location.origin}/authorize`,
-    webAPI: 'https://api.spotify.com/v1',
-    scopes: [
-      'user-read-email',
-      'user-read-private',
-      'streaming'
-    ]
-  }
-}
+    webAPI: "https://api.spotify.com/v1",
+    scopes: ["user-read-email", "user-read-private", "streaming"],
+  },
+};
