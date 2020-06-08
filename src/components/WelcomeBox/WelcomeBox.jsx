f2110ec3fee123fb4bb816f58c61e825455e4e5e
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 19:48:16 -0300

    test: Adiciona id para testes faltantes

--- src/components/WelcomeBox/WelcomeBox.jsx
+++ src/components/WelcomeBox/WelcomeBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./WelcomeBox.scss";
 
 const WelcomeBox = ({ name }) => (
-  <div className="welcome-box" data-test-id="welcome-box">
+  <div className="welcome-box" data-testid="welcome-box">
     <div className="container">
       <span>
         Seja bem vindo, <strong>{name}</strong>
