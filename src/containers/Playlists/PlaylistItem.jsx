71925f66ced69b9d39d4c169a467e02cffd70242
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 18:27:43 -0300

    feat: Adiciona playlists

--- src/containers/Playlists/PlaylistItem.jsx
+++ src/containers/Playlists/PlaylistItem.jsx
@@ -1,6 +1,24 @@
-import React  from 'react';
+import React from "react";
+import Ink from "react-ink";
+import { Link } from "react-router-dom";
 
-const PlaylistItem = ({ categoryId, description, id, image, name, path }) => (<div className="playlists__item" />)
+const PlaylistItem = ({ categoryId, description, id, image, name, path }) => (
+  <div className="playlists__item" data-testid="playlist">
+    <Link
+      className="playlists__item__link"
+      style={{ backgroundImage: `url(${image.url})` }}
+      title={name}
+      to={`${path}/${categoryId}/${id}`}
+    >
+      <Ink />
+    </Link>
+
+    <p className="playlists__item__description">
+      <strong>{name}</strong>
+
+      {description}
+    </p>
+  </div>
+);
 
 export default PlaylistItem;
-
--- src/containers/Playlists/Playlists.jsx
+++ src/containers/Playlists/Playlists.jsx
@@ -1,8 +1,35 @@
-import React  from 'react';
+import React from "react";
 
-import './Playlists.scss';
+import "./Playlists.scss";
 
-const Playlists = ({ data, categoryName, categoryId, isLoading, path }) => (<div className="playlists" />);
+import { Loading, RouteHeader } from "../../components";
+import PlaylistItem from "./PlaylistItem";
+
+const Playlists = ({ data, categoryName, categoryId, isLoading, path }) => (
+  <div className="playlists" data-testid="playlists">
+    <div className="container">
+      <RouteHeader categoryName={categoryName} path={path} />
+
+      {isLoading ? (
+        <Loading text="Carregando playlists..." />
+      ) : (
+        <div className="playlists__content">
+          {data.length &&
+            data.map((playlist) => (
+              <PlaylistItem
+                categoryId={categoryId}
+                description={playlist.description}
+                id={playlist.id}
+                image={playlist.images[0]}
+                key={playlist.id}
+                name={playlist.name}
+                path={path}
+              />
+            ))}
+        </div>
+      )}
+    </div>
+  </div>
+);
 
 export default Playlists;
-
