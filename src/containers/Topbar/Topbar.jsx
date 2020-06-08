c890c2b4593b99e904a9e86258813aebea1d2d70
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 18:11:59 -0300

    feat: Adiciona topbar

--- src/containers/Topbar/Topbar.jsx
+++ src/containers/Topbar/Topbar.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './Topbar.scss';
+import "./Topbar.scss";
 
-const Topbar = () => (<header className="topbar" />);
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { Logo } from "../../components";
+
+const Topbar = () => {
+  const user = useSelector((state) => state.user);
+
+  return (
+    <header className="topbar" data-testid="topbar">
+      <div className="container">
+        <Link to="/dashboard">
+          <Logo />
+        </Link>
+
+        <div className="user">
+          <span className="user__name">{user.name}</span>
+
+          <figure className="user__thumb">
+            {user.thumb ? (
+              <img src={user.thumb} alt={`foto de perfil de ${user.name}`} />
+            ) : null}
+          </figure>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Topbar;
