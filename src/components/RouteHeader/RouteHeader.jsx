2b8fd72772201b01a88aa1fc9977504e7590a654
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 19:45:16 -0300

    feat: Adiciona navegação entre histórico de rotas

--- src/components/RouteHeader/RouteHeader.jsx
+++ src/components/RouteHeader/RouteHeader.jsx
@@ -1,8 +1,20 @@
-import React  from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './RouteHeader.scss';
+import { RiArrowLeftLine } from "react-icons/ri";
 
-const RouterHeader = ({ categoryName, path }) => (<div className="route-header" />);
+import "./RouteHeader.scss";
+
+const RouterHeader = ({ categoryName, path }) => (
+  <div className="route-header" data-testid="route-header">
+    <div className="route-header__title-group">
+      <Link to={path} className="back-button">
+        <RiArrowLeftLine />
+      </Link>
+
+      <span className="route-header__title">{categoryName}</span>
+    </div>
+  </div>
+);
 
 export default RouterHeader;
-
