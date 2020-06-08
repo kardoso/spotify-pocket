3c3e0247de5ebc59fc5cf500e54a552a7a98531d
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 18:19:29 -0300

    feat: Adiciona container de categorias

--- src/containers/Categories/Categories.jsx
+++ src/containers/Categories/Categories.jsx
@@ -1,13 +1,34 @@
-import React  from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
 
-import { Loading } from '../../components';
+import { Loading } from "../../components";
 
-import CategoryItem from './CategoryItem';
+import CategoryItem from "./CategoryItem";
 
-import './Categories.scss';
+import "./Categories.scss";
 
-const Categories = ({ data, isLoading, url }) => (<div className="categories" />);
+const Categories = ({ data, isLoading, url }) => (
+  <div className="categories" data-testid="categories">
+    <div className="container">
+      <h3 className="categories__title">Categorias</h3>
+
+      {isLoading ? (
+        <Loading text="Carregando..." />
+      ) : (
+        <div className="categories__content">
+          {data.length &&
+            data.map((category) => (
+              <CategoryItem
+                icon={category.icons[0]}
+                id={category.id}
+                key={category.id}
+                name={category.name}
+                url={url}
+              />
+            ))}
+        </div>
+      )}
+    </div>
+  </div>
+);
 
 export default Categories;
-
