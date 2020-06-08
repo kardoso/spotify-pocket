6e2bb067d384d348b24f226e12caec39317b8666
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 19:38:12 -0300

    feat: Adiciona container de autorização

--- src/containers/Authorize/Authorize.jsx
+++ src/containers/Authorize/Authorize.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './Authorize.scss';
+import "./Authorize.scss";
+
+import { Loading } from "../../components";
+import backgroundImage from "../../assets/images/app-intro-1.jpg";
 
 const Authorize = () => {
-  return (<div className="callback" />);
-}
+  return (
+    <div
+      className="callback"
+      data-testid="callback"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="container">
+        <Loading text="Autenticando..." />
+      </div>
+    </div>
+  );
+};
 
 export default Authorize;
-
