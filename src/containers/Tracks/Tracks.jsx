2e912121bdeba1be6fbaf6f4e74466ad04d19276
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 18:42:03 -0300

    feat: Adiciona tracks

--- src/containers/Tracks/Tracks.jsx
+++ src/containers/Tracks/Tracks.jsx
@@ -1,8 +1,27 @@
-import React  from 'react';
+import React from "react";
 
-import './Tracks.scss';
+import "./Tracks.scss";
 
-const Tracks = ({ categoryName, data, isLoading, path }) => (<div className="tracks" data-testid="tracks" />);
+import { Loading, RouteHeader } from "../../components";
+import Track from "./Track";
+
+const Tracks = ({ categoryName, data, isLoading, path }) => (
+  <div className="tracks" data-testid="tracks">
+    <div className="container">
+      <RouteHeader categoryName={categoryName} path={path} />
+
+      {isLoading ? (
+        <Loading text="Carregando tracks..." />
+      ) : (
+        <div className="tracks__content">
+          {data.length &&
+            data.map(({ track }, index) => (
+              <Track key={`${index} - ${track.id}`} track={track} />
+            ))}
+        </div>
+      )}
+    </div>
+  </div>
+);
 
 export default Tracks;
-
