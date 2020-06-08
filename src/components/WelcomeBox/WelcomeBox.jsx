b32ebe08efebbe40422a3c02be5e20e1bbc5acdb
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-08 19:44:52 -0300

    feat: Adiciona mensagem de boas vindas

--- src/components/WelcomeBox/WelcomeBox.jsx
+++ src/components/WelcomeBox/WelcomeBox.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './WelcomeBox.scss';
+import "./WelcomeBox.scss";
 
 const WelcomeBox = ({ name }) => (
-  <div className="welcome-box" />
+  <div className="welcome-box" data-test-id="welcome-box">
+    <div className="container">
+      <span>
+        Seja bem vindo, <strong>{name}</strong>
+      </span>
+    </div>
+  </div>
 );
 
 export default WelcomeBox;
