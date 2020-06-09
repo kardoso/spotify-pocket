e067b84415d2d83a740b8bc7b427a862323a83f4
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-06-09 00:30:53 -0300

    fix: Adiciona Client Id

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   spotify: {
     authorizationURL: "https://accounts.spotify.com/authorize",
-    clientId: `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`,
+    clientId: "b8f2cb33c1eb46aeb2bd45b441300237",
     redirectUrl: `${window.location.origin}/authorize`,
     webAPI: "https://api.spotify.com/v1",
     scopes: ["user-read-email", "user-read-private", "streaming"],
